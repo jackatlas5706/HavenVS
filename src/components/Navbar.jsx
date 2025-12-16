5f469e29994fadb9b9b8a353f52fca70355e5701
jackatlas5706 <ryan.haug@students.mot.k12.de.us>
2025-12-16 08:32:35 -0500

    Create Navbar.jsx

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+// src/components/Navbar.jsx
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <nav>
+            <h1>MMORPG Game</h1>
+            <ul>
+                <li>Home</li>
+                <li>Characters</li>
+                <li>Inventory</li>
+                <li>Settings</li>
+            </ul>
+        </nav>
+    );
+};
+
+export default Navbar;
