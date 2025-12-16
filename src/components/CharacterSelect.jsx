da5d8092d21fbc0d1a46bd6f2569ab5d6bb5c69b
jackatlas5706 <ryan.haug@students.mot.k12.de.us>
2025-12-16 08:33:00 -0500

    Create CharacterSelect.jsx

---
+++ src/components/CharacterSelect.jsx
@@ -0,0 +1,15 @@
+// src/components/CharacterSelect.jsx
+import React from 'react';
+
+const CharacterSelect = () => {
+    return (
+        <div>
+            <h2>Select Your Character</h2>
+            <button>Warrior</button>
+            <button>Mage</button>
+            <button>Rogue</button>
+        </div>
+    );
+};
+
+export default CharacterSelect;
