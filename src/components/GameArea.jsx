7e338f19e25dcf240fe2103a6007b6b58b187732
jackatlas5706 <ryan.haug@students.mot.k12.de.us>
2025-12-16 08:33:23 -0500

    Create GameArea.jsx

---
+++ src/components/GameArea.jsx
@@ -0,0 +1,13 @@
+// src/components/GameArea.jsx
+import React from 'react';
+
+const GameArea = () => {
+    return (
+        <div>
+            <h2>Game Area</h2>
+            <p>Map and game content will be here.</p>
+        </div>
+    );
+};
+
+export default GameArea;
