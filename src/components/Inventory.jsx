96e61d7722c5fa34a1fc1fce79fc5eb0300745e5
jackatlas5706 <ryan.haug@students.mot.k12.de.us>
2025-12-16 08:33:43 -0500

    Create Inventory.jsx

---
+++ src/components/Inventory.jsx
@@ -0,0 +1,13 @@
+// src/components/Inventory.jsx
+import React from 'react';
+
+const Inventory = () => {
+    return (
+        <div>
+            <h2>Inventory</h2>
+            <p>Your items will be displayed here.</p>
+        </div>
+    );
+};
+
+export default Inventory;
